69c5cabf65c6ad47c23e4cdeaa74b9ac909e41c4
marlonmp <marlonmp214@protonmail.com>
2024-02-16 00:18:29 -0500

    tab navigator added

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import HomeStackNavigator from './HomeStackNavigator';
+import PokedexStackNavigator from './PokedexStackNavigator';
+import FavoritesStackNavigator from './FavoriteStackNavigator';
+
+
+const Tab = createBottomTabNavigator();
+
+const tabs = [
+  {
+    name: 'home',
+    component: HomeStackNavigator,
+    options: {
+      tabBarLabel: 'Home',
+      headerShown: false,
+      tabBarIcon: ({ color, size }) => <Icon name="home" {...{color, size}} />,
+    },
+  },
+  {
+    name: 'pokedex',
+    component: PokedexStackNavigator,
+    options: {
+      tabBarLabel: '',
+      headerShown: false,
+      tabBarIcon: () => <PockedexIcon />,
+    },
+  },
+  {
+    name: 'favorites',
+    component: FavoritesStackNavigator,
+    options: {
+      tabBarLabel: 'Favorites',
+      headerShown: false,
+      tabBarIcon: ({ color, size }) => <Icon name="favorite" {...{color, size}} />,
+    },
+  },
+];
+
+export default function TabNavigation () {
+  return (
+    <Tab.Navigator>
+      {tabs.map((tab, i) => <Tab.Screen key={i} {...tab} />)}
+    </Tab.Navigator>
+  );
+}
+
+function PockedexIcon() {
+  return <Image source={require('../assets/pokeball.png')} style={{ width: 85, height: 85, top: -18 }} />;
+}
